b0d8659a2273de0803cb2679a4d9a238416e3ca5
nimabi66 <77653967+nimabi66@users.noreply.github.com>
2024-02-16 17:17:25 +0800

    Update commit.sh

--- commit.sh
+++ commit.sh
@@ -5,8 +5,8 @@ if test -z "$(git status -s)"; then
   echo 'no commit'
   exit 0;
 else
-  git config --local user.email "1982989137@qq.com"
-  git config --local user.name "flow2000"
+  git config --local user.email "nimabi66@xzmail.cloudns.org"
+  git config --local user.name "nimabi66"
   git pull origin master
   git add data/*
   git commit -m "update bing json"
